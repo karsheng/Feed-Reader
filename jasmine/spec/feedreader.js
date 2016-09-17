04eef1b4c544c4c5bdd02934ed8e962cc3d080f0
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-09-17 16:53:15 +0800

    fixed indentation

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,8 +22,8 @@ $(function() {
          * page?
          */
         it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+              expect(allFeeds).toBeDefined();
+              expect(allFeeds.length).not.toBe(0);
         });
 
 
@@ -32,10 +32,10 @@ $(function() {
          * and that the URL is not empty.
          */
          it('URLs are defined and not empty', function() {
-            allFeeds.forEach(function(feed) {
-              expect(feed.url).toBeDefined();
-              expect(feed.url.length).not.toBe(0);
-            });
+              allFeeds.forEach(function(feed) {
+                  expect(feed.url).toBeDefined();
+                  expect(feed.url.length).not.toBe(0);
+              });
          });
 
         /* TODO: Write a test that loops through each feed
@@ -43,10 +43,10 @@ $(function() {
          * and that the name is not empty.
          */
          it('names are defined and not empty', function() {
-            allFeeds.forEach(function(feed) {
-              expect(feed.name).toBeDefined();
-              expect(feed.name.length).not.toBe(0);
-            });
+              allFeeds.forEach(function(feed) {
+                  expect(feed.name).toBeDefined();
+                  expect(feed.name.length).not.toBe(0);
+              });
          });
     });
 
@@ -59,15 +59,15 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-         // NOTE: App toggles .menu-hidden to hide/unhide the menu from the body.
+        // NOTE: App toggles .menu-hidden to hide/unhide the menu from the body.
          var hidden;
          beforeEach(function() {
-           hidden = $('body').hasClass('menu-hidden');
+              hidden = $('body').hasClass('menu-hidden');
          });
 
          it('is hidden by default', function() {
-           // expect body has the class of .menu-hidden, which hides the menu.
-           expect(hidden).toBe(true);
+              // expect body has the class of .menu-hidden, which hides the menu.
+              expect(hidden).toBe(true);
          });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -76,27 +76,27 @@ $(function() {
           */
 
           it('menu visible when clicked', function() {
-            // ensure menu is hidden
-            if (!hidden) {
-                $('body').addClass('menu-hidden');
-            }
-            // click on the menu
-            $('.menu-icon-link').trigger('click');
+              // ensure menu is hidden
+              if (!hidden) {
+                  $('body').addClass('menu-hidden');
+              }
+              // click on the menu
+              $('.menu-icon-link').trigger('click');
 
-            // expect the .menu-hidden to be removed i.e. menu visible
-            expect($('body').hasClass('menu-hidden')).not.toBe(true);
+              // expect the .menu-hidden to be removed i.e. menu visible
+              expect($('body').hasClass('menu-hidden')).not.toBe(true);
           });
 
           it('menu hidden when clicked', function() {
-            // ensure menu is visible
-            if (hidden) {
-                $('body').removeClass('menu-hidden');
-            }
-            // click on the menu
-            $('.menu-icon-link').trigger('click');
+              // ensure menu is visible
+              if (hidden) {
+                  $('body').removeClass('menu-hidden');
+              }
+              // click on the menu
+              $('.menu-icon-link').trigger('click');
 
-            // expect the .menu-hidden to be present i.e. menu hidden
-            expect($('body').hasClass('menu-hidden')).toBe(true);
+              // expect the .menu-hidden to be present i.e. menu hidden
+              expect($('body').hasClass('menu-hidden')).toBe(true);
           });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
@@ -107,17 +107,17 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-         beforeEach(function(done){
-           loadFeed(0, function() {
-             done();
-           });
-         });
+          beforeEach(function(done){
+              loadFeed(0, function() {
+                  done();
+              });
+          });
 
-         it('loadFeed function is called and completed: at least a single .entry element within the .feed container.', function(done) {
-           // expects to find content in the .entry element in the .feed container
-           expect($('.feed').find('.entry').length).not.toBe(0);
-           done();
-         });
+          it('loadFeed function is called and completed: at least a single .entry element within the .feed container.', function(done) {
+              // expects to find content in the .entry element in the .feed container
+              expect($('.feed').find('.entry').length).not.toBe(0);
+              done();
+          });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function(){
@@ -130,19 +130,19 @@ $(function() {
 
          // load two different feeds
          beforeEach(function(done){
-           loadFeed(0, function() {
-             oldFeed = $('.feed').html();
-           });
-           loadFeed(1, function() {
-             newFeed = $('.feed').html();
-             done();
-           });
-         });
+              loadFeed(0, function() {
+                  oldFeed = $('.feed').html();
+              });
+              loadFeed(1, function() {
+                  newFeed = $('.feed').html();
+                  done();
+              });
+          });
 
          // when loadFeed() finished loading asynchronously, expect oldFeed to not be equal to newFeed
          it('content changes when a new feed is loaded', function(done) {
-           expect(oldFeed).not.toEqual(newFeed);
-           done();
-         });
+              expect(oldFeed).not.toEqual(newFeed);
+              done();
+          });
     });
 }());
